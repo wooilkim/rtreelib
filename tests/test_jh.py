7835354b238e2d5dd6cf472a52912a82cddd10fa
hoonww <jh981113@naver.com>
2020-12-09 14:43:24 +0900

    RTree diagram(datasize : 100)

--- tests/test_jh.py
+++ tests/test_jh.py
@@ -1,33 +1,23 @@
 from rtreelib import RTree, Rect
 from rtreelib.diagram import create_rtree_diagram
 import csv
-import os
-os.environ["PATH"] += os.pathsep + 'C:\Program Files (x86)\Graphviz 2.44.1\bin'
 
+t = RTree()
+M = 0.0000001
 
-# Create an RTree instance with some sample data
-t = RTree(max_entries=4)
-t.insert('a', Rect(0, 0, 3, 3))
-t.insert('b', Rect(2, 2, 4, 4))
-t.insert('c', Rect(1, 1, 2, 4))
-t.insert('d', Rect(8, 8, 10, 10))
-t.insert('e', Rect(7, 7, 9, 9))
+f = open('normal.csv', 'r', encoding='utf-8')
+val = csv.reader(f)
+for i in val:
+    identity, category, x, y = i
 
-# Create a diagram of the R-tree structure
-#create_rtree_diagram(t)
-
-
-f=open('normal_.csv', 'r', encoding='utf-8')
-reader=csv.reader(f)
-for i in reader:
-    id, category, x, y = i
-
-    if id == "id":
+    if identity == "id":
         continue
-    print(x+" "+y)
+    if int(identity) > 10:
+        break
+    x = float(x)
+    y = float(y)
+    t.insert(identity, Rect(x, y, x + M, y + M))
 
-    #
-    # processing
-    #
+create_rtree_diagram(t)
 
 f.close()
